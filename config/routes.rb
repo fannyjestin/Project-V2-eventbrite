b781ec01c2770a8503da6fddd0584a5f04e59a46
Fanny Jestin <f.jestin@tbs-education.org>
2019-11-05 16:38:44 +0100

    versioon2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  root "static_pages#index"
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
+  root "events#index"
+
+  resources :events
+  resources :teams
+  resources :contacts
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
